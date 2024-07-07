1c7e0f248bf861dbad7cf63916478429055115d8
Sylvain Jimenez <sylvain.jimenez54290@gmail.com>
2024-07-07 14:31:34 +0200

    API doc

--- config/bundles.php
+++ config/bundles.php
@@ -8,5 +8,7 @@ return [
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
-    
+    Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
 ];
